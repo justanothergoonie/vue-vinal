a04ef54c2e9f8d27bfbf99376145ca84645888fb
Cody J Powell <codyj.powell@outlook.com>
2020-09-05 13:58:05 -0400

    added font

--- src/components/Ghibli-Search.vue
+++ src/components/Ghibli-Search.vue
@@ -1,67 +1,68 @@
 <template>
-	<div class="ghibli">
-		<h1>Ghibli</h1>
-		<img alt="Vue logo" src="../assets/logo.png" />
-		<form v-on:submit.prevent>
-			<select name="subject" v-model="newSubject">
-				<option value>Subject</option>
-				<option value="films">Films</option>
-				<option value="people">People</option>
-				<option value="locations">Locations</option>
-				<option value="species">Species</option>
-				<option value="vehicles">Vehicles</option>
-			</select>
-			<input
-				type="text"
-				name="character"
-				placeholder="Something Ghibli"
-				v-model="newSpecSub"
-			/>
-			<button
-				class="submit"
-				@click="
+  <div class="ghibli">
+    <h1>Ghibli</h1>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <form v-on:submit.prevent>
+      <select name="subject" v-model="newSubject">
+        <option value>Subject</option>
+        <option value="films">Films</option>
+        <option value="people">People</option>
+        <option value="locations">Locations</option>
+        <option value="species">Species</option>
+        <option value="vehicles">Vehicles</option>
+      </select>
+      <input type="text" name="character" placeholder="Something Ghibli" v-model="newSpecSub" />
+      <button
+        class="submit"
+        @click="
 					$emit('do-search');
 					('gimmie-ghibli-giphy-gifs');
 				"
-			>
-				Submit
-			</button>
-		</form>
-	</div>
+      >Submit</button>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-	name: 'Ghibli-API',
-	props: ['searchResults'],
+  name: "Ghibli-API",
+  props: ["searchResults"],
 
-	data: function() {
-		return {
-			newSubject: '',
-			newSpecSub: '',
-		};
-	},
-	method: {
-		nameSake: function() {
-			console.log('GhibliSearch.vue sent a search to App.vue');
-			this.$emit('searching-ghibli');
-		},
-	},
-	watch: {
-		newSubject: function() {
-			console.log(
-				'GhibliSearch.vue saw a change in the selected subject',
-				this.newSubject
-			);
-			this.$emit('subject-search', this.newSubject);
-		},
-		newSpecSub: function() {
-			console.log(
-				'GhibliSearch.vue saw a change in the specSubject',
-				this.newSpecSub
-			);
-			this.$emit('specSub-search', this.newSpecSub);
-		},
-	},
+  data: function() {
+    return {
+      newSubject: "",
+      newSpecSub: ""
+    };
+  },
+  method: {
+    nameSake: function() {
+      console.log("GhibliSearch.vue sent a search to App.vue");
+      this.$emit("searching-ghibli");
+    }
+  },
+  watch: {
+    newSubject: function() {
+      console.log(
+        "GhibliSearch.vue saw a change in the selected subject",
+        this.newSubject
+      );
+      this.$emit("subject-search", this.newSubject);
+    },
+    newSpecSub: function() {
+      console.log(
+        "GhibliSearch.vue saw a change in the specSubject",
+        this.newSpecSub
+      );
+      this.$emit("specSub-search", this.newSpecSub);
+    }
+  }
 };
 </script>
+<style >
+.ghibli {
+  font-family: Ghibli;
+}
+form {
+  margin-bottom: 5%;
+}
+</style>
--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,50 +1,52 @@
 <template>
-	<ul class="results" v-if="searchResults.length > 0">
-		<li v-for="result in searchResults" :key="result.id">
-			<h1>{{ result.title ? result.title : result.name }}</h1>
-			<div v-if="result.gender">
-				<p>
-					{{ result.name }} is a {{ result.age }} year old
-					{{ result.gender }} with {{ result.eye_color }} eyes and
-					{{ result.hair_color }} hair
-				</p>
-			</div>
-			<div v-if="result.director">
-				<p>
-					{{ result.title }} was released in
-					{{ result.release_date }} and was directed by
-					{{ result.director }}. Summary: {{ result.description }}
-				</p>
-			</div>
-			<div v-if="result.climate">
-				<p>Climate: {{ result.climate }}</p>
-				<p>Terrain: {{ result.terrain }}</p>
-			</div>
-			<div v-if="result.classification">
-				<p>Classification: {{ result.classification }}</p>
-				<p>Eye Color(s): {{ result.eye_colors }}</p>
-				<p>Hair Color(s): {{ result.hair_colors }}</p>
-			</div>
-			<div v-if="result.vehicle_class">
-				<p>Length: {{ result.length }}</p>
-				<p>Vehical Class: {{ result.vehicle_class }}</p>
-			</div>
-		</li>
-	</ul>
+  <ul class="results" v-if="searchResults.length > 0">
+    <li v-for="result in searchResults" :key="result.id">
+      <h1>{{ result.title ? result.title : result.name }}</h1>
+      <div v-if="result.gender">
+        <p>
+          {{ result.name }} is a {{ result.age }} year old
+          {{ result.gender }} with {{ result.eye_color }} eyes and
+          {{ result.hair_color }} hair
+        </p>
+      </div>
+      <div v-if="result.director">
+        <p>
+          {{ result.title }} was released in
+          {{ result.release_date }} and was directed by
+          {{ result.director }}. Summary: {{ result.description }}
+        </p>
+      </div>
+      <div v-if="result.climate">
+        <p>Climate: {{ result.climate }}</p>
+        <p>Terrain: {{ result.terrain }}</p>
+      </div>
+      <div v-if="result.classification">
+        <p>Classification: {{ result.classification }}</p>
+        <p>Eye Color(s): {{ result.eye_colors }}</p>
+        <p>Hair Color(s): {{ result.hair_colors }}</p>
+      </div>
+      <div v-if="result.vehicle_class">
+        <p>Length: {{ result.length }}</p>
+        <p>Vehical Class: {{ result.vehicle_class }}</p>
+      </div>
+    </li>
+  </ul>
 </template>
 
 <script>
 export default {
-	name: 'Results',
-	props: ['searchResults', 'specSub'],
+  name: "Results",
+  props: ["searchResults", "specSub"]
 };
 </script>
 
 <style>
 ul {
-	list-style: none;
-	display: flex;
-	flex-direction: column;
-	padding: 0;
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  padding: 0;
+  font-family: Ghibli;
+  font-size: 1.3rem;
 }
 </style>
